964aaa65af4a5e42776abf83ff5d82a0e9d3dde0
Ryn-coding <aryanvaghela53@gmail.com>
2026-03-25 23:25:42 +0530

    PT Event planner

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <p>&copy; 2023 Event Planner. All rights reserved.</p>
+                <p>Contact us at info@eventplanner.com</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
